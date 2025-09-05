0a86b76c7b8330ac6636bb9183b65df9e5bb3c93
이재환 <36800639+ComeBiga@users.noreply.github.com>
2025-09-05 16:44:41 +0900

    PushPull 오브젝트 타고 오를 때 더 부드럽게 수정

--- Project_IS/Assets/2.Scripts/HdrpScripts/PlayerClimbObjectState.cs
+++ Project_IS/Assets/2.Scripts/HdrpScripts/PlayerClimbObjectState.cs
@@ -9,17 +9,23 @@ public class PlayerClimbObjectState : PlayerStateBase
     [SerializeField] private float _lerpYPosDuration = .2f;         // 애니메이션 높이로 보정해주는 시간
     [SerializeField] private float _climbUpYSpeed = 2f;
     [SerializeField] private float _climbUpZSpeed = 3f;
+    [SerializeField][Range(0f, 1f)] private float _climbUpZDelay = .6f;
 
     [Header("Climb Down")]
     [SerializeField] private float _lerpZPosDuration = .2f;         // 애니메이션 위치로 보정해주는 시간
     [SerializeField] private float _climbDownRotateSpeed = 2f;
     [SerializeField] private float _climbDownYSpeed = 1f;
     [SerializeField] private float _climbDownZSpeed = 1f;
+    [SerializeField][Range(0f, 1f)] private float _climbDownEndTime = .6f;
 
     private Animator mAnimator;
     private PushPullObject mPushPullObject;
     private bool mbClimbing = false;                // 오르기/내리기 중인지
     private bool mbClimbUp;                         // 오르기 인지 내리기 인지
+
+    private bool mbActiveIK = false;
+    private Vector3 mTargetIKPos;
+    private float mHandIKWeight = 1f;
 
     public override void Initialize(PlayerController controller)
     {
@@ -34,6 +40,9 @@ public class PlayerClimbObjectState : PlayerStateBase
         mController.Movement.SetUseGravity(false);
         mController.Movement.SetColliderActive(false);
         mbClimbing = true;
+
+        mController.Animator.onAnimationIK -= updateAnimatorIK;
+        mController.Animator.onAnimationIK += updateAnimatorIK;
 
         if (mbClimbUp)
         {
@@ -54,6 +63,8 @@ public class PlayerClimbObjectState : PlayerStateBase
         mController.Animator.SetHorizontal(0f);
         // 오르기 인지 내리기 인지 AnimatorController에서 체크는 아래 parameter로 한다.
         mController.Animator.SetVertical(0f);
+
+        mController.Animator.onAnimationIK -= updateAnimatorIK;
     }
 
     public override void Tick()
@@ -103,19 +114,60 @@ public class PlayerClimbObjectState : PlayerStateBase
 
         Quaternion targetRotation = Quaternion.Euler(Vector3.zero);
 
+        mController.Animator.SetVelocityY(0f);
+
+        // IK Hand
+        mTargetIKPos = transform.position;
+        mTargetIKPos.y = ppoBounds.max.y;
+        mTargetIKPos.z = ppoBounds.min.z;
+        mHandIKWeight = .5f;
+        // mbActiveIK = true;
+
         float timer = 0f;
+        float lerpDuration = _lerpRotationDuration;
+
+        Vector3 startPos = transform.position;
+        // Vector3 startPos = targetPos;
+        Quaternion startRot = transform.rotation;
+
+        //while (timer < lerpDuration)
+        //{
+        //    float t = timer / lerpDuration;
+        //    // transform.position = Vector3.Lerp(startPos, targetPos, t);
+
+        //    transform.rotation = Quaternion.Lerp(startRot, targetRotation, t);
+
+        //    // mHandIKWeight = t;
+
+        //    timer += Time.deltaTime;
+        //    yield return null;
+        //}
+
+        //transform.position = targetPos;
+        transform.rotation = targetRotation;
+        mController.Animator.SetVelocityY(1f);
+
+        mbActiveIK = false;
+
+        //AnimatorStateInfo animatorStateInfo = mAnimator.GetCurrentAnimatorStateInfo(0);
+
+        //yield return new WaitUntil(() => animatorStateInfo.IsTag("ClimbUp"));
+
+        //transform.position = targetPos;
+
+        timer = 0f;
 
         while (mbClimbing)
         {
-            if (timer < _lerpYPosDuration)
-            {
-                transform.position = Vector3.Lerp(transform.position, targetPos, timer / _lerpYPosDuration);
-                // 지금 _lerpYPosDuration과 _lerpRotationDuration 값이 같기 때문에 같은 if문 안에 작성해줌
-                // 값이 달라질거면 if문을 나눠줘야됨, 같을거면 변수를 통합해줘야됨
-                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, timer / _lerpRotationDuration);
+            //if (timer < _lerpYPosDuration)
+            //{
+            //    transform.position = Vector3.Lerp(transform.position, targetPos, timer / _lerpYPosDuration);
+            //    // 지금 _lerpYPosDuration과 _lerpRotationDuration 값이 같기 때문에 같은 if문 안에 작성해줌
+            //    // 값이 달라질거면 if문을 나눠줘야됨, 같을거면 변수를 통합해줘야됨
+            //    transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, timer / _lerpRotationDuration);
 
-                timer += Time.deltaTime;
-            }
+            //    timer += Time.deltaTime;
+            //}
 
             AnimatorStateInfo animatorStateInfo = mAnimator.GetCurrentAnimatorStateInfo(0);
 
@@ -126,9 +178,19 @@ public class PlayerClimbObjectState : PlayerStateBase
             }
 
             Vector3 deltaPosition = mAnimator.deltaPosition;
+            deltaPosition.x = 0f;
             deltaPosition.y *= _climbUpYSpeed;
             // 캐릭터 z 위치가 0까지만 이동하게 조건을 줌
-            deltaPosition.z *= (transform.position.z < 0f) ? _climbUpZSpeed : 0f;
+            if (animatorStateInfo.normalizedTime > _climbUpZDelay)
+            {
+                deltaPosition.z *= (transform.position.z < 0f) ? _climbUpZSpeed : 0f;
+                mController.Movement.UpdateRotation();
+            }
+            else
+            {
+                deltaPosition.z = 0f;
+            }
+
             transform.position += deltaPosition;
 
             yield return null;
@@ -187,10 +249,31 @@ public class PlayerClimbObjectState : PlayerStateBase
             }
             else
             {
-                transform.rotation = Quaternion.Euler(Vector3.zero);
+                if(animatorStateInfo.normalizedTime > _climbDownEndTime)
+                {
+                    mController.Movement.SetUseGravity(true);
+                    mController.Movement.SetColliderActive(true);
+                    mController.Movement.UpdateRotation();
+                }
+                else
+                {
+                    transform.rotation = Quaternion.Euler(Vector3.zero);
+                }
             }
+
 
             yield return null;
         }
+    }
+
+    private void updateAnimatorIK()
+    {
+        if (!mbActiveIK)
+            return;
+
+        mAnimator.SetIKPosition(AvatarIKGoal.LeftHand, mTargetIKPos);
+        mAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, mHandIKWeight);
+        mAnimator.SetIKPosition(AvatarIKGoal.RightHand, mTargetIKPos);
+        mAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, mHandIKWeight);
     }
 }
