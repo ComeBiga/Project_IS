66b820ff12b9fd439ff3cc98d1c07fefed462eb6
이재환 <36800639+ComeBiga@users.noreply.github.com>
2025-09-11 21:31:46 +0900

    오브젝트 밀어 넘어뜨리기

--- Project_IS/Assets/2.Scripts/HdrpScripts/PlayerPushPullState.cs
+++ Project_IS/Assets/2.Scripts/HdrpScripts/PlayerPushPullState.cs
@@ -7,6 +7,12 @@ public class PlayerPushPullState : PlayerStateBase
     private Animator mAnimator;
     private PushPullObject mPushPullObject;
 
+    private bool mbPushPull = true;
+
+    private bool mbActiveIK = false;
+    private Vector3 mLeftHandIKPos;
+    private Vector3 mRightHandIKPos;
+
     public override void Initialize(PlayerController controller)
     {
         base.Initialize(controller);
@@ -16,18 +22,28 @@ public class PlayerPushPullState : PlayerStateBase
 
     public override void EnterState()
     {
+        mbActiveIK = false;
+
         // mController.Movement.PushPull(Vector2.zero, 0f);
         transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        // mPushPullObject.SetFriction(false);
+
+        //mController.Animator.onAnimationIK -= updateAnimationIK;
+        //mController.Animator.onAnimationIK += updateAnimationIK;
+
+        // StartCoroutine(eHandIKPos());
     }
 
     public override void ExitState()
     {
+        // mPushPullObject.SetFriction(true);
 
+        // mController.Animator.onAnimationIK -= updateAnimationIK;
     }
 
     public override void Tick()
     {
-        if (!mController.InputHandler.IsInteracting)
+        if (!mController.InputHandler.IsInteracting || !mbPushPull)
         {
             mController.StateMachine.SwitchState(PlayerStateMachine.EState.Move);
             return;
@@ -37,27 +53,64 @@ public class PlayerPushPullState : PlayerStateBase
         // Debug.Log(mAnimator.velocity);
         Vector3 animationVelocity = mAnimator.velocity;
         animationVelocity.z = 0f;
-        mController.Movement.SetVelocity(animationVelocity);
+        // mController.Movement.SetVelocity(animationVelocity);
 
         float moveInputX = mController.InputHandler.MoveInput.x;
         float pushPullMultiplier = 0f;
 
         if(moveInputX > .1f)
         {
+            mPushPullObject.SetFriction(false);
             pushPullMultiplier = 1f;
         }
         else if(moveInputX < -.1f)
         {
+            mPushPullObject.SetFriction(false);
             pushPullMultiplier = -1f;
         }
+        else
+        {
+            mPushPullObject.SetFriction(true);
+        }
 
+        mController.Movement.SetVelocity(pushPullMultiplier * Vector3.right * .8f);
         mController.Animator.SetHorizontal(pushPullMultiplier);
 
-        mPushPullObject.PushPull(animationVelocity);
+        // Debug.Log(animationVelocity);
+        // mPushPullObject.PushPull(animationVelocity);
+        mbPushPull = mPushPullObject.PushPull(pushPullMultiplier * Vector3.right * .8f);
     }
 
     public void SetPushPullObject(PushPullObject pushPullObject)
     {
         mPushPullObject = pushPullObject;
+    }
+
+    private IEnumerator eHandIKPos()
+    {
+        yield return new WaitUntil(() => mAnimator.GetCurrentAnimatorStateInfo(0).IsTag("PushPull"));
+
+        Bounds bounds = mPushPullObject.BoxCollider.bounds;
+
+        mLeftHandIKPos = mAnimator.GetBoneTransform(HumanBodyBones.LeftHand).position;
+        mLeftHandIKPos.z = bounds.min.z;
+        mPushPullObject.HandlePointL.position = mLeftHandIKPos;
+
+        mRightHandIKPos = mAnimator.GetBoneTransform(HumanBodyBones.RightHand).position;
+        mRightHandIKPos.z = bounds.min.z;
+        mPushPullObject.HandlePointR.position = mRightHandIKPos;
+
+        mbActiveIK = true;
+    }
+
+    private void updateAnimationIK()
+    {
+        if(!mbActiveIK)
+            return;
+
+        mAnimator.SetIKPosition(AvatarIKGoal.LeftHand, mPushPullObject.HandlePointL.position);
+        mAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1f);
+        mAnimator.SetIKPosition(AvatarIKGoal.RightHand, mPushPullObject.HandlePointR.position);
+        mAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1f);
     }
 }
--- Project_IS/Assets/2.Scripts/HdrpScripts/PushPullObject.cs
+++ Project_IS/Assets/2.Scripts/HdrpScripts/PushPullObject.cs
@@ -7,16 +7,40 @@ public class PushPullObject : InteractableObject
 {
     public float PushPullSpeed => _pushPullSpeed;
     // public BoxCollider BoxCollider => mBoxCollider;
+    public Transform HandlePointL => _handlePointL;
+    public Transform HandlePointR => _handlePointR;
 
     [Header("PushPullObject")]
     [SerializeField] private float _pushPullSpeed = 1f;
+    [SerializeField] private PhysicMaterial _matNoFriction;
+    [SerializeField] private Transform _handlePointL;
+    [SerializeField] private Transform _handlePointR;
 
     private Rigidbody mRigidbody;
     // private BoxCollider mBoxCollider;
 
-    public void PushPull(Vector3 velocity)
+    public void SetFriction(bool value)
     {
-        mRigidbody.velocity = velocity;
+        if (value)
+            mBoxCollider.material = null;
+        else
+            mBoxCollider.material = _matNoFriction;
+    }
+
+    public bool PushPull(Vector3 velocity)
+    {
+        Vector3 finalVelocity = mRigidbody.velocity;
+        finalVelocity.x = Mathf.Abs(mRigidbody.velocity.x) > Mathf.Abs(velocity.x) ? mRigidbody.velocity.x : velocity.x;
+        mRigidbody.velocity = finalVelocity;
+        // mRigidbody.AddForce(velocity, ForceMode.Force);
+
+        // mRigidbody.AddForceAtPosition(velocity, mBoxCollider.bounds.center, ForceMode.Force);
+        Debug.Log($"velocity {mRigidbody.velocity}, angular velocity {mRigidbody.angularVelocity}");
+
+        //if (Mathf.Abs(mRigidbody.velocity.y) > 1f)
+        //    return false;
+
+        return true;
     }
 
     // Start is called before the first frame update
